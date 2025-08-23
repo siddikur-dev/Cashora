62f70b2530ca68ea23baa635dcf95434df152657
siddikur-dev <siddikur.dev@gmail.com>
2025-08-23 10:46:26 +0600

    loggin successfully to go to new window

--- script.js
+++ script.js
@@ -1,4 +1,42 @@
 document.getElementById("login-btn").addEventListener("click", function (e) {
-    e.preventDefault()
-  console.log("Button CLicked");
+  e.preventDefault();
+
+  //Our Provided Number and pin
+  const mobileNumber = "01751920983";
+  const pinNumber = 1234;
+
+  //Mobile Number And Login Pin Number get
+  const MobileNumberValue = document.getElementById("mobileNumber").value;
+  const pinNumberValue = document.getElementById("pinNumber").value;
+
+  //   convert string to number: Mobile and  PinNumber
+  const convertedMobileNumberValue = parseInt(MobileNumberValue);
+  const convertedPinNumberValue = parseInt(pinNumberValue);
+  //   Our Provided Number
+  const convertedOurProvidedNumber = parseInt(mobileNumber);
+
+// alertShowingInnerHTMLCreate
+const showingAlert = document.getElementById("messageContainer");
+
+if (
+  convertedOurProvidedNumber === convertedMobileNumberValue &&
+  pinNumber === convertedPinNumberValue
+) {
+//   showingAlert.innerHTML = `
+//     <div role="alert" class="alert alert-success alert-soft">
+//       <span>Credential Matched</span>
+//     </div>
+//   `;
+  console.log("match");
+  window.location.href = "./home.html";
+  alert("Credential Matched")
+} else {
+//   showingAlert.innerHTML = `
+//     <div role="alert" class="alert alert-error alert-soft">
+//       <span>Invalid Credential!</span>
+//     </div>
+//   `;
+  alert("Invalid Credential !")
+}
+
 });
