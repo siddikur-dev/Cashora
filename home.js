0ff6e2fe85c913e4b1b7c88b77a77c44d686ce03
siddikur-dev <siddikur.dev@gmail.com>
2025-08-23 17:34:34 +0600

    some button feature implement

--- home.js
+++ home.js
@@ -24,3 +24,55 @@ document.getElementById("addMoneyBtn").addEventListener("click", function (e) {
     alert("Add amount maximum: 10k");
   }
 });
+
+//featured toggle
+// addMoney
+document
+  .getElementById("addMoneyButton")
+  .addEventListener("click", function () {
+    document.getElementById("cashOutParent").style.display = "none";
+    document.getElementById("addMoneyParent").style.display = "block";
+  });
+//   cashOut
+document.getElementById("cashOutButton").addEventListener("click", function () {
+  document.getElementById(
+    "addMoneyParent",
+    "transferMoneyParent",
+    "getBonusParent",
+    "payBillButton"
+  ).style.display = "none";
+  document.getElementById("cashOutParent").style.display = "block";
+});
+// transfer money
+document
+  .getElementById("transferMoneyButton")
+  .addEventListener("click", function () {
+    document.getElementById(
+      "addMoneyParent",
+      "cashOutParent",
+      "getBonusParent",
+      "payBillButton"
+    ).style.display = "none";
+    document.getElementById("transferMoneyParent").style.display = "block";
+  });
+document
+  .getElementById("getBonusButton")
+  .addEventListener("click", function () {
+    document.getElementById(
+      "addMoneyParent",
+      "cashOutParent",
+      "getBonusParent",
+      "payBillButton",
+      "transferMoneyParent"
+    ).style.display = "none";
+    document.getElementById("getBonusParent").style.display = "block";
+  });
+document.getElementById("payBillButton").addEventListener("click", function () {
+  document.getElementById(
+    "addMoneyParent",
+    "cashOutParent",
+    "transferMoneyParent",
+    "getBonusParent"
+  ).style.display = "none";
+  document.getElementById("payBIll").style.display = "block";
+});
